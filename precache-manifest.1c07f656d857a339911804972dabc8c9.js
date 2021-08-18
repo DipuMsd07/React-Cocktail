e9ca3dd23358a71905b76656176df2ea40236db6
DipuMsd7 <dsarode013@gmail.com>
2021-08-18 15:11:46 +0530

    Updates

---
+++ precache-manifest.1c07f656d857a339911804972dabc8c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "529d818789295a50f36ba82787febf6b",
+    "url": "/React-Cocktail/index.html"
+  },
+  {
+    "revision": "9e746827cb764635093a",
+    "url": "/React-Cocktail/static/css/main.5c529f91.chunk.css"
+  },
+  {
+    "revision": "e8d3e0d0b7469c75a09d",
+    "url": "/React-Cocktail/static/js/2.fe9845cd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Cocktail/static/js/2.fe9845cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e746827cb764635093a",
+    "url": "/React-Cocktail/static/js/main.10f025f8.chunk.js"
+  },
+  {
+    "revision": "e1160d4dbe7df8860499",
+    "url": "/React-Cocktail/static/js/runtime-main.dea6a4a9.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/React-Cocktail/static/media/logo.9a3d2645.svg"
+  }
+]);
